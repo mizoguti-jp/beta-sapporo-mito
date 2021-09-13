721528e1ad30d2309496151cb24a3a97fe391354
Satsuki-mito <89887226+Satsuki-mito@users.noreply.github.com>
2021-09-13 15:45:56 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -135,35 +135,7 @@ var order = 0;
 var score = 1;
 var trueList = [0,3,4,1,2,5,8,7,6];
 
-$(function() {
-  $("#0").click(function() {
-    $("#0").css("background-color", "gray");
-  });
-  $("#1").click(function() {
-    $("#1").css("background-color", "gray");
-  });
-  $("#2").click(function() {
-    $("#2").css("background-color", "gray");
-  });
-  $("#3").click(function() {
-    $("#3").css("background-color", "gray");
-  });
-  $("#4").click(function() {
-    $("#4").css("background-color", "gray");
-  });
-  $("#5").click(function() {
-    $("#5").css("background-color", "gray");
-  });
-  $("#6").click(function() {
-    $("#6").css("background-color", "gray");
-  });
-  $("#7").click(function() {
-    $("#7").css("background-color", "gray");
-  });
-  $("#8").click(function() {
-    $("#8").css("background-color", "gray");
-  });
-});
+
 
 function game(input){
   if(input == 9){
